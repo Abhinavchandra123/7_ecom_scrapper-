ad5ae7ed52ad77f7cd2bc03dbeb6c8a30366a819
Abhinavchandra123 <aa30424@gmail.com>
2024-08-15 20:52:22 +0530

    no sandbox edit

--- ecom6.py
+++ ecom6.py
@@ -23,7 +23,7 @@ class ModelSportScraper:
         # ****************** Use this for server hosting ***********************
         options.add_argument("--headless") 
         options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
+        # options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         # **********************************************************************
         
